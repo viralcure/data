7b2dabeba76485f84dd9fb88a8b55716dc15966e
Velrino <denismagalhaesvelrino@gmail.com>
2020-03-28 15:26:30 -0300

    Create missions_page.ts

---
+++ missions_page.ts
@@ -0,0 +1,10 @@
+{
+    "header": {
+        "title": "em uma pandemia tudo é imprevisível. Por isso, precisamos estar preparados para tudo. o tempo todo. o quanto antes. as missões que aparecem aqui, uma vez completadas, vão nos ajudar nisso.",
+        "text": "Da possibilidade de resgatar o maior número de infectados com mais rapizes e eficiencia, passando pela certeza de que teremos equipamentos de segurança para atender e trabalhar, até e garantia de fornecimento de alimentação para quem está dedicando suas vidas e intermináveis horas de trabalho na pesquisa de soluções e cuidado à população, entre coisas."
+    },
+    "footer" : {
+        "title": "em tempos emerciais de crise, não só a demanda por materiais e insumos mas os valores",
+        "text": "*a viralcure não faz custódia de recursos, e nem cobra quaisquer taxas de quem doa ou recebe doações"
+    }
+}
